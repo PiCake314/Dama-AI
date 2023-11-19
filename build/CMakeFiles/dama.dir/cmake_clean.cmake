7d43757451a260c7bf168286537a71cfacfb68d5
Pie :3! <94876844+PiCake314@users.noreply.github.com>
2023-11-18 22:44:36 -0700

    initial commit

---
+++ build/CMakeFiles/dama.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dama.dir/dama.cpp.o"
+  "CMakeFiles/dama.dir/dama.cpp.o.d"
+  "dama"
+  "dama.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dama.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
